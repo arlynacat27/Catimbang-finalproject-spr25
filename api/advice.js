5d12c6cb0574435fbc72b2230640444490caf4b6
Arlyna Catimbang <arlyna.catimbang@gmail.com>
2025-05-14 23:22:40 -0400

    submit4

--- api/advice.js
+++ api/advice.js
@@ -4,9 +4,9 @@ export default async function handler(req, res) {
     try {
       const response = await fetch("https://api.adviceslip.com/advice");
       const data = await response.json();
-      res.status(200).json(data[0]);
+      res.status(200).json(data.slip);
     } catch (err) {
-      res.status(500).json({ error: "Failed to fetch quote." });
+      res.status(500).json({ error: "Failed to fetch advice." });
     }
   } else {
     res.setHeader("Allow", ["GET"]);
